27ec20384ebe9cc3ec800c72e5cfa4b3737144a1
salioudunn11 <salioudunn11@gmail.comclean>
2026-06-10 13:53:03 -0400

    send button printing

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,53 @@ return (
   <>
     <h1>College Schedule Builder</h1>
     <p>It's time to build your schedule.</p>
+
+{
+  courses.map((courseItem, index) => (
+    <div key={index}>
+      <p>{courseItem.course}</p>
+      <p>{courseItem.day}</p>
+      <p>{courseItem.startTime} - {courseItem.endTime}</p>
+    </div>
+  ))
+}
   
     <form onSubmit={(event) => {
       event.preventDefault();
+
+      // const newCourses = course;
+
+
+      const newCourse = {
+      course: course,
+      day: day,
+      startTime: startTime,
+      endTime: endTime
+};
+      setCourses([...courses, newCourse]);
+
+
+
+
     }}>
              
-        <input name='course' 
-               id='course' 
-               value={course}
-               onChange={(event) => setCourse(event.target.value)}
-        />
+        <select id="course"
+                value={course}
+                onChange={(event) => setCourse(event.target.value)}
+        >
+          <option value="">Select Course</option>
+          <option value="Intro to Data">Intro to Data</option>
+          <option value="Web Programming">Web Programming</option>
+          <option value="French">French</option>
+          <option value="Database">Database</option>
+          <option value="Security">Security</option>
 
-        <select id="day">
+        </select>
+
+        <select id="day"
+                value={day}
+                onChange={(event) => setDay(event.target.value)}
+                >
           <option value="">Select Day</option>
           <option value="Monday">Monday</option>
           <option value="Tuesday">Tuesday</option>
@@ -38,16 +73,51 @@ return (
         </select>
 
 
-        <input name='startTime' 
-               id='startTime' 
-               value={startTime}
-               onChange={(event) => setStartTime(event.target.value)}
-        />
-        <input name='endTime' 
-               id='endTime' 
-               value={endTime}
-               onChange={(event) => setEndTime(event.target.value)}
-        />
+        <select id="startTime"
+                value={startTime}
+                onChange={(event) => setStartTime(event.target.value)}
+        >
+          <option value="">Starting Time</option>
+          <option value="8 AM">8 AM</option>
+          <option value="9 AM">9 AM</option>
+          <option value="10 AM">10 AM</option>
+          <option value="11 AM">11 AM</option>
+          <option value="12 PM">12 PM</option>
+          <option value="1 PM">1 PM</option>
+          <option value="2 PM">2 PM</option>
+          <option value="3 PM">3 AM</option>
+          <option value="4 PM">4 PM</option>
+          <option value="5 PM">5 AM</option>
+          <option value="6 PM">6 PM</option>
+          <option value="7 PM">7 AM</option>
+          <option value="8 PM">8 PM</option>          
+
+
+
+        </select>
+
+        <select id="endTime"
+                value={endTime}
+                onChange={(event) => setEndTime(event.target.value)}
+                >
+          <option value="">Ending Time</option>
+          <option value="9 AM">9 AM</option>
+          <option value="10 AM">10 AM</option>
+          <option value="11 AM">11 AM</option>
+          <option value="12 PM">12 PM</option>
+          <option value="1 PM">1 PM</option>
+          <option value="2 PM">2 PM</option>
+          <option value="3 PM">3 AM</option>
+          <option value="4 PM">4 PM</option>
+          <option value="5 PM">5 AM</option>
+          <option value="6 PM">6 PM</option>
+          <option value="7 PM">7 AM</option>
+          <option value="8 PM">8 PM</option>
+          <option value="9 PM">9 PM</option>
+          <option value="10 PM">10 PM</option>             
+
+        </select>
+
 
         <button type='submit'>
             send
